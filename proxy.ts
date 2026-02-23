4d326b46a6232c089594edd093c19b98a4590ca2
Shanshan Chen <shanshanc.chen@gmail.com>
2026-02-23 13:50:50 +0800

    rename middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { COOKIE_NAME, computeToken } from "@/lib/auth";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const secret = process.env.AUTH_SECRET;
 
